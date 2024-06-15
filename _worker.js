1bdd6338d5d03fd684e6b061bed0067ab9851beb
xiutrim <52069724+xiutrim@users.noreply.github.com>
2024-06-15 11:29:57 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -8,7 +8,7 @@ let userID = '90cd4a77-141a-43c9-991b-08263cfe9c10';
 
 let proxyIP = '';// 小白勿动，该地址并不影响你的网速，这是给CF代理使用的。'cdn.xn--b6gac.eu.org, cdn-all.xn--b6gac.eu.org, workers.cloudflare.cyou'
 
-let sub = '';// 留空则使用内置订阅
+let sub = '200848a7-a9be-4546-bcc4-79b9bb10304a.cmliussss.workers.dev';// 留空则使用内置订阅
 let subconverter = 'url.v1.mk';// clash订阅转换后端，目前使用肥羊的订阅转换功能。自带虚假uuid和host订阅。
 let subconfig = "https://raw.githubusercontent.com/cmliu/ACL4SSR/main/Clash/config/ACL4SSR_Online_Full_MultiMode.ini"; //订阅配置文件
 
